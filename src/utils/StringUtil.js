a6aeda63b2c9652c361eb4e59cc74766715860e5
mengshuang <mengshuang@58ganji.com>
2018-11-14 18:00:31 +0800

    数据转换TEXT处理

--- src/utils/StringUtil.js
+++ src/utils/StringUtil.js
@@ -1,13 +1,15 @@
 /**
  * 将name由下划线转成驼峰
  * @param name
- * @param startUp
+ * @param startUp 第一个字母是否大写
  */
 export function toCamel(name, startUp = false) {
-  var arr = name.split('_')
-  var str = arr[0]
+  var arr = name.split('_');
+  var str = arr[0];
   if (startUp) {
     str = arr[0].charAt(0).toUpperCase() + arr[0].slice(1)
+  }else{
+    str = arr[0].charAt(0).toLowerCase() + arr[0].slice(1)
   }
   for (var i = 1; i < arr.length; i++) {
     str += arr[i].charAt(0).toUpperCase() + arr[i].slice(1)
@@ -47,7 +49,8 @@ export function toJavaType(type) {
     case 'CHAR':
     case 'VARCHAR':
     case 'TINYTEXT':
-      return 'String'
+    case 'TEXT':
+      return 'String';
     default:
       return 'undefined'
 
